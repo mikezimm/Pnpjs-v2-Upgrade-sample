414f1cc8de91f7a2b8b9c9e7b3107e4b780d2255
mikezimm <49648086+mikezimm@users.noreply.github.com>
2022-10-13 01:51:58 -0400

    Add CodeTour for main web part class

--- src/webparts/v2NpmBanner/IV2NpmBannerWebPartProps.ts
+++ src/webparts/v2NpmBanner/IV2NpmBannerWebPartProps.ts
@@ -8,34 +8,37 @@
 import { exportIgnorePropsFPS, } from '@mikezimm/npmfunctions/dist/HelpPanelOnNPM/onNpm/BannerInterface';
 import { importBlockPropsFPS } from '@mikezimm/npmfunctions/dist/HelpPanelOnNPM/onNpm/BannerInterface';
 
-import { IMinBannerUIProps, IMinPinMeProps, IMinPandoramicProps, IMinBannerThemeProps, IMinCustomHelpProps, 
-  IMinPageStyleProps, IMinBannerUtilityProps, IMinFPSLegacyProps } from "@mikezimm/npmfunctions/dist/HelpPanelOnNPM/onNpm/BannerInterface";
+import {
+  IMinBannerUIProps, IMinPinMeProps, IMinPandoramicProps, IMinBannerThemeProps, IMinCustomHelpProps,
+  IMinPageStyleProps, IMinBannerUtilityProps, IMinFPSLegacyProps
+} from "@mikezimm/npmfunctions/dist/HelpPanelOnNPM/onNpm/BannerInterface";
 
 
 //Specific for this web part
-export const exportIgnorePropsThis : string[] = [ ];
+export const exportIgnorePropsThis: string[] = [];
 
-console.log( 'exportIgnorePropsFPS', exportIgnorePropsFPS );
+console.log('exportIgnorePropsFPS', exportIgnorePropsFPS);
 
-export const exportIgnoreProps : string[] = [ ...exportIgnorePropsFPS, ...exportIgnorePropsThis  ];
+export const exportIgnoreProps: string[] = [...exportIgnorePropsFPS, ...exportIgnorePropsThis];
 
 //These props will not be imported even if they are in one of the change arrays above (fail-safe)
 //This was done so user could not manually insert specific props to over-right fail-safes built in to the webpart
 
 //Specific for this web part
-export const importBlockPropsThis : string[] = [ 'showSomeProps' ];
+export const importBlockPropsThis: string[] = ['showSomeProps'];
 
-export const importBlockProps : string[] = [ ...importBlockPropsFPS, ...importBlockPropsThis ];
+export const importBlockProps: string[] = [...importBlockPropsFPS, ...importBlockPropsThis];
 
-export const changePropertyGroupX : string[] = [ 'showSomeProps', 'showCustomProps' , 'showOOTBProps' , 'showApprovalProps' , 'propsTitleField', 'propsExpanded', 'selectedProperties' ];
+export const changePropertyGroupX: string[] = ['showSomeProps', 'showCustomProps', 'showOOTBProps', 'showApprovalProps', 'propsTitleField', 'propsExpanded', 'selectedProperties'];
 
 // export interface IV2NpmBannerWebPartProps extends IMinWPBannerProps {
-  /**
-   * Extend with portions of FPS Props that are needed
-   * 
-   */
+/**
+ * Extend with portions of FPS Props that are needed
+ * 
+ */
 export interface IV2NpmBannerWebPartProps extends IMinBannerUIProps, IMinPinMeProps, IMinPandoramicProps, IMinBannerThemeProps, IMinCustomHelpProps, IMinPageStyleProps, IMinBannerUtilityProps, IMinFPSLegacyProps {
   [key: string]: any;
+  
   description: string;
 
 }
